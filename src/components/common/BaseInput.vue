287f60a8b72481b0f160249d446cdbec60d17d0a
Md Arman <armanar6712@gmail.com>
2025-08-23 23:51:57 +0530

    feat: implement main layout with navbar and footer, add base button and input components

---
+++ src/components/common/BaseInput.vue
@@ -0,0 +1,12 @@
+<template>
+  <input :class="['base-input', inputClasses]" v-bind="$attrs" />
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  inputClasses: {
+    type: String,
+    default: '',
+  },
+})
+</script>
---
+++ src/components/layout/BaseFooter.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="text-center mt-4 h-10">Desing and made by Arman with ❤️</div>
+</template>
